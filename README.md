c3da12c86d2c164e35f364c17698bf689811fd7d
Ritesh <riteshpal@gmail.com>
2025-11-13 22:00:35 +0530

    Practice using API

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project About using API link to connect html and js file to extract data
+API link:
+    . Cat Facts: https://catfact.ninja/fact
+    . cat IMage: https://api.thecatapi.com/v1/images/search
+
+Cdn link for Load Axios:
+        https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js
---
+++ app.js
@@ -0,0 +1,43 @@
+let url = "https://catfact.ninja/fact";
+let url2 = "https://api.thecatapi.com/v1/images/search";
+
+// 🐾 Get Cat Fact
+let CatFact = document.querySelector("#CatFact");
+CatFact.addEventListener("click", async () => {
+  let fact = await getCatFact();
+  console.log(fact);
+  let p = document.querySelector("#Result");
+  p.innerText = fact;
+});
+
+async function getCatFact() {
+  try {
+    let response = await axios.get(url);
+    return response.data.fact;
+  } catch (err) {
+    console.log("Error -", err);
+    return "No facts found.";
+  }
+}
+
+// 🐾 Get Cat Image
+let CatImage = document.querySelector("#CatImage");
+CatImage.addEventListener("click", async () => {
+  let image = await getCatImage();
+  console.log(image);
+
+  let imgTag = document.querySelector("#image");
+  imgTag.setAttribute("src", image);
+  imgTag.width = 300;
+  imgTag.alt = "Cute Cat 🐱";
+});
+
+async function getCatImage() {
+  try {
+    let response = await axios.get(url2);
+    return response.data[0].url;
+  } catch (err) {
+    console.log("Error -", err);
+    return "No image found.";
+  }
+}
